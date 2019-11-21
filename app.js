9c90b277596141991a6860ea70c4411d1afda109
UmedJadhav <umedj.formal@gmail.com>
2019-11-22 00:31:32 +0530

    Added loading.gif and logic to display it

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+const UIresults = document.getElementById('results');
+const UIloading = document.getElementById('loading');
+
+document.getElementById('loan-form').addEventListener('submit',function(e){
+    UIresults.style.display = 'none';
+    UIloading.style.display ='block';
+    setTimeout(calculateResults(e),2000);
+    e.preventDefault();
+});
 
 function calculateResults(e){
     const UIamount = document.getElementById('amount');
@@ -16,6 +24,8 @@ function calculateResults(e){
     const monthly = (principle*x*calculatedInterest)/(x-1);
 
     if(isFinite(monthly)){
+        UIresults.style.display = 'block';
+        UIloading.style.display = 'none';
         UImonthlyPayment.value = monthly.toFixed(2);
         UItotalPayment.value = (monthly * calculatedPayments).toFixed(2);
         UItotalInterest.value = ((monthly*calculatedPayments) - principle).toFixed(2);
@@ -26,6 +36,8 @@ function calculateResults(e){
 }
 
 function showError(error){
+    UIloading.style.display = 'none';
+    UIresults.style.display = 'none';
     const errorDiv = document.createElement('div');
     errorDiv.className = 'alert alert-danger';
     errorDiv.appendChild(document.createTextNode(error));
